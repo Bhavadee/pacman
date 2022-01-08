9cda4c82567aff8b9632f59834fef17a83a224e9
bhvadeepreddy <bhavadeep2003@gmail.com>
2022-01-08 22:04:07 +0530

    pacman

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-let width = 28
-const grid = document.querySelector(".grid")
-const scoreId = document.querySelector("#score")
+const width = 28
+const grid = document.querySelector('.grid')
+const scoreDisplay = document.getElementById('score')
 let squares = []
-
+let score = 0
 // 0 - pacdots
 // 1 - wall
 // 2 - ghost lair
@@ -58,6 +58,10 @@ function createBoard() {
       {
           squares[i].classList.add("wall")
       }
+      else if(layout[i] === 2)
+      {
+          squares[i].classList.add("ghost-lair")
+      }
       else if(layout[i] === 3)
       {
           squares[i].classList.add("power-pellet")
@@ -78,22 +82,68 @@ function control(e) {
     squares[pacmanCurrentIndex].classList.remove('pacman')
     switch(e.keyCode) {
         case 40:
-        console.log('pressed down')
-        if ( !squares[pacmanCurrentIndex + width].classList.contains('wall') &&  pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+        
+        if ( !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') && 
+            !squares[pacmanCurrentIndex + width].classList.contains('wall') 
+        &&  pacmanCurrentIndex + width < width * width) 
+        pacmanCurrentIndex += width
         break
         case 38:
-        console.log('pressed up')
-        if (  !squares[pacmanCurrentIndex - width].classList.contains('wall') &&pacmanCurrentIndex - width >=0) pacmanCurrentIndex -= width
+       
+        if (!squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&   
+            !squares[pacmanCurrentIndex - width].classList.contains('wall') 
+        &&pacmanCurrentIndex - width >=0) 
+        pacmanCurrentIndex -= width
         break
         case 37: 
         console.log('pressed left')
-        if( !squares[pacmanCurrentIndex - 1].classList.contains('wall') && pacmanCurrentIndex % width !==0) pacmanCurrentIndex -=1
+        if(!squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') && 
+             !squares[pacmanCurrentIndex - 1].classList.contains('wall') 
+             && pacmanCurrentIndex % width !==0) 
+             pacmanCurrentIndex -=1
+
+             if (pacmanCurrentIndex === 364) {
+                pacmanCurrentIndex = 391
+            }
         break
         case 39:
         console.log('pressed right')
-        if(  !squares[pacmanCurrentIndex + 1].classList.contains('wall') && pacmanCurrentIndex % width < width -1) pacmanCurrentIndex +=1
+        if( !squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&  
+            !squares[pacmanCurrentIndex + 1].classList.contains('wall') && pacmanCurrentIndex % width < width -1)
+             pacmanCurrentIndex +=1
+
+             if (pacmanCurrentIndex === 391) {
+                pacmanCurrentIndex = 364
+            }
         break
     }
     squares[pacmanCurrentIndex].classList.add('pacman')
+    pacDotEaten()
 }
 document.addEventListener('keyup', control)
+
+
+
+
+function pacDotEaten() {
+    if (squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+        squares[pacmanCurrentIndex].classList.remove('pac-dot')
+        score++
+        scoreDisplay.innerHTML = score
+    }
+}
+
+class Ghost {
+    constructor(className, startIndex, speed) {
+        this.className = className
+        this.startIndex = startIndex
+        this.speed = speed
+    }
+}
+
+ghosts = [
+    new Ghost('blinky', 348, 250),
+    new Ghost('pinky', 376, 400),
+    new Ghost('inky', 351, 300),
+    new Ghost('clyde', 379, 500)
+]
